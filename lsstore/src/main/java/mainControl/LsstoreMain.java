f3e470b7e5048303fa33c0e58fb5ca60651e0983
Kabaki Antony <kabaki.antony@gmail.com>
2025-06-09 13:05:30 +0300

    refactor the main entry file for the project

--- lsstore/src/main/java/mainControl/LsstoreMain.java
+++ lsstore/src/main/java/mainControl/LsstoreMain.java
@@ -1,10 +1,10 @@
-package main;
+package mainControl;
 
 /**
  *
  * @author ls
  */
-public class Lsstore {
+public class LsstoreMain {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
