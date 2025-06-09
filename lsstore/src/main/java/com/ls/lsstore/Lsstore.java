42708110fffff76d63e30c117638d38a40226f64
Kabaki Antony <kabaki.antony@gmail.com>
2025-06-09 09:41:36 +0300

    create project directory

---
+++ lsstore/src/main/java/com/ls/lsstore/Lsstore.java
@@ -0,0 +1,12 @@
+package com.ls.lsstore;
+
+/**
+ *
+ * @author ls
+ */
+public class Lsstore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
