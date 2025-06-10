7f285bd9a3e3b24874915e2bd54dc8083d74ad5b
Kabaki Antony <kabaki.antony@gmail.com>
2025-06-10 12:28:21 +0300

    update UserDAO to use passwordutil

--- lsstore/src/main/java/dao/UserDAO.java
+++ lsstore/src/main/java/dao/UserDAO.java
@@ -3,6 +3,8 @@ package dao;
 import model.Role;
 import model.User;
 import utils.DBConnection;
+import utils.LSStoreLogger;
+import utils.PasswordUtil;
 
 import java.sql.*;
 
@@ -25,23 +27,25 @@ public class UserDAO {
                 );
             }
         } catch (SQLException e) {
-            e.printStackTrace(); // or use logger
+            LSStoreLogger.error( 0, "FETCH_USER", "UserDAO", "Failed to get user: " + username, e);
         }
         return null;
     }
 
     public boolean create(User user) {
+        String hashedPassword = PasswordUtil.hashPassword(user.getPassword());
         String sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getPassword()); // already hashed
+            stmt.setString(2, hashedPassword);
             stmt.setString(3, user.getRole().name());
 
             return stmt.executeUpdate() > 0;
         } catch (SQLException e) {
-            e.printStackTrace(); // or use logger
+            LSStoreLogger.error( 0, "CREATE_USER", "UserDAO", "Failed to create user: " + user.getUsername(), e);
+
         }
         return false;
     }
