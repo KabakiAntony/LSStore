54d00505e96e1ea1f6033ba2b47bc9ddadc3609c
Kabaki Antony <kabaki.antony@gmail.com>
2025-06-10 13:36:38 +0300

    create passwordutil file to hash and verify password during user creation and login

--- lsstore/src/main/java/utils/PasswordUtil.java
+++ lsstore/src/main/java/utils/PasswordUtil.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utils;
 
-/**
- *
- * @author ls
- */
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public class PasswordUtil {
-    
+
+    private static final int COST = 12;
+
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.withDefaults().hashToString(COST, plainPassword.toCharArray());
+    }
+
+    public static boolean verifyPassword(String plainPassword, String hashedPassword) {
+        BCrypt.Result result = BCrypt.verifyer().verify(plainPassword.toCharArray(), hashedPassword);
+        return result.verified;
+    }
 }
