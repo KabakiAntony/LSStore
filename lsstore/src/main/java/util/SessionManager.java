02fa6ac642a8c6fad64c9ccba755c22f5be05578
Kabaki Antony <kabaki.antony@gmail.com>
2025-06-12 12:46:12 +0300

    add session manager

---
+++ lsstore/src/main/java/util/SessionManager.java
@@ -0,0 +1,29 @@
+package util;
+
+
+import model.User;
+
+/**
+ *
+ * @author ls
+ */
+public class SessionManager {
+    private static User currentUser;
+
+    public static void login(User user) {
+        currentUser = user;
+    }
+
+    public static void logout() {
+        currentUser = null;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+    
+}
